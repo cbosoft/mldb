4a7282b7708db10207f04f29d58cef128ea3f5ec
Chris B <cbosoft@protonmail.com>
2022-08-27 12:39:12 +0100

    Fix test for postresql backed; improve cleanup

--- tests/db_test.py
+++ tests/db_test.py
@@ -1,42 +1,46 @@
-import os
+from datetime import datetime
 
 from mldb.config import CONFIG
 from mldb.database import Database
 
 
 def test_db():
-    CONFIG.db_path = 'test.db'
-    CONFIG.database = 'Test'
 
-    if os.path.isfile(CONFIG.db_path):
-        os.remove(CONFIG.db_path)
+    CONFIG.host = 'tyke.db.elephantsql.com'
+    CONFIG.database = CONFIG.user = 'fgwfjfjg'
+    CONFIG.password = 'fd8TXM0-bmDAM46WVpD0LbtWw33lzP1g'
+    CONFIG.port = 5432
+
+    today = datetime.now().strftime('%Y%m%d%H%M%S')
 
     with Database() as db:
-        db.set_exp_status('TEST_1', 'TRAINING')
-        db.add_metric_value('TEST_1', 'RMSE', 1, 1.0)
-        db.set_exp_status('TEST_1', 'COMPLETE')
-        db.add_metric_value('TEST_1', 'RMSE', 2, 0.5)
+        db.set_exp_status(f'{today}_TEST_1', 'TRAINING')
+        db.add_metric_value(f'{today}_TEST_1', 'RMSE', 1, 1.0)
+        db.set_exp_status(f'{today}_TEST_1', 'COMPLETE')
+        db.add_metric_value(f'{today}_TEST_1', 'RMSE', 2, 0.5)
 
-        db.add_metric_value('TEST_2', 'RMSE', 1, 0.5)
-        db.set_exp_status('TEST_2', 'TRAINING')
-        db.add_metric_value('TEST_2', 'RMSE', 2, 0.25)
-        db.set_exp_status('TEST_2', 'COMPLETE')
+        db.add_metric_value(f'{today}_TEST_2', 'RMSE', 1, 0.5)
+        db.set_exp_status(f'{today}_TEST_2', 'TRAINING')
+        db.add_metric_value(f'{today}_TEST_2', 'RMSE', 2, 0.25)
+        db.set_exp_status(f'{today}_TEST_2', 'COMPLETE')
 
-        db.add_metric_value('TEST_3', 'RMSE', 1, 0.25)
-        db.set_exp_status('TEST_3', 'TRAINING')
-        db.add_metric_value('TEST_3', 'RMSE', 2, 0.125)
+        db.add_metric_value(f'{today}_TEST_3', 'RMSE', 1, 0.25)
+        db.set_exp_status(f'{today}_TEST_3', 'TRAINING')
+        db.add_metric_value(f'{today}_TEST_3', 'RMSE', 2, 0.125)
 
         db.conn.commit()
 
         db.cursor.execute(
             'SELECT STATUS.EXPID FROM \
             STATUS INNER JOIN METRICS ON STATUS.EXPID=METRICS.EXPID \
-            WHERE STATUS.STATUS=\'COMPLETE\' AND METRICS.KIND=\'RMSE\' \
-            ORDER BY METRICS.VALUE ASC LIMIT 1;')
+            WHERE STATUS.STATUS=\'COMPLETE\' \
+            AND METRICS.KIND=\'RMSE\' \
+            AND METRICS.EXPID LIKE %s \
+            ORDER BY METRICS.VALUE ASC LIMIT 1;', (f'{today}_TEST_%',))
 
         rows = db.cursor.fetchall()
         assert len(rows) == 1
-        assert rows[0][0] == 'TEST_2'
+        assert rows[0][0] == f'{today}_TEST_2'
 
-        db.cursor.execute('drop table metrics;')
+        db.cursor.execute('DELETE FROM METRICS WHERE EXPID LIKE %s;', (f'{today}_TEST_%',))
         db.conn.commit()
