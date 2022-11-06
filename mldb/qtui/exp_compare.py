c58521bbe78d2e3c04e3ae30127f8bb2442742a8
Christopher Boyle <chrismjboyle@gmail.com>
2022-11-06 13:45:11 -0500

    Add option to group experiments in exp compare table

--- mldb/qtui/exp_compare.py
+++ mldb/qtui/exp_compare.py
@@ -1,10 +1,71 @@
 from typing import List
 
 import numpy as np
-from PySide6.QtWidgets import QWidget, QDialog, QVBoxLayout, QTabWidget
+from PySide6.QtCore import Signal
+from PySide6.QtWidgets import (
+    QWidget, QDialog,
+    QVBoxLayout, QHBoxLayout,
+    QSpinBox,
+    QTabWidget, QTableWidget, QTableWidgetItem, QHeaderView
+)
 
 from .plot_widget import PlotWidget
 from .db_iop import DBExpDetails, DBExpMetrics
+
+
+class GroupTable(QTableWidget):
+
+    groups_changed = Signal(dict)
+
+    def __init__(self):
+        super().__init__()
+        self.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.expids = []
+        self.groups = {}
+
+    def add_expid(self, expid):
+        self.expids.append(expid)
+        self.refresh()
+
+    def refresh(self):
+        self.clear()
+        self.setColumnCount(2)
+        self.setRowCount(len(self.expids))
+
+        for i, expid in enumerate(self.expids):
+            self.setItem(i, 0, QTableWidgetItem(expid))
+            groupbox = QSpinBox()
+            self.setCellWidget(i, 1, groupbox)
+            groupbox.valueChanged.connect(lambda v, e=expid: self.set_group(e, v))
+
+    def set_group(self, expid, group):
+        self.groups[expid] = group
+        if group == 0:
+            if expid in self.groups:
+                self.groups.pop(expid)
+        self.groups_changed.emit(self.groups)
+
+
+def plot_widget_and_table(grouping_table=False):
+    w = QWidget()
+    w.layout = QVBoxLayout(w)
+    plt = PlotWidget()
+    w.layout.addWidget(plt)
+    if grouping_table:
+        hw = QWidget()
+        hw.layout = QHBoxLayout(hw)
+        w.layout.addWidget(hw)
+        tbl = QTableWidget()
+        tbl.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        tbl_grouping = GroupTable()
+        hw.layout.addWidget(tbl)
+        hw.layout.addWidget(tbl_grouping)
+        return w, plt, tbl, tbl_grouping
+    else:
+        tbl = QTableWidget()
+        tbl.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        w.layout.addWidget(tbl)
+        return w, plt, tbl
 
 
 class ExpCompareDialog(QDialog):
@@ -27,13 +88,16 @@ class ExpCompareDialog(QDialog):
         self.loss_plot.axes.set_title('Loss v Epoch')
         self.plots.addTab(self.loss_plot, 'Loss v Epoch')
 
-        self.metrics_low_plot = PlotWidget()
-        self.metrics_low_plot.axes.set_title('Metrics (lower is better)')
-        self.plots.addTab(self.metrics_low_plot, 'Metrics (errors)')
+        final_metrics_low_widget, self.final_metrics_low_plot, self.final_metrics_low_table, self.final_metrics_low_groups = plot_widget_and_table(grouping_table=True)
+        self.low_metrics_grouping = {}
+        self.final_metrics_low_groups: GroupTable
+        self.final_metrics_low_groups.groups_changed.connect(self.low_grouping_changed)
+        self.final_metrics_low_plot.axes.set_title('Metrics (lower is better)')
+        self.plots.addTab(final_metrics_low_widget, 'Final Metrics (errors)')
 
-        self.metrics_high_plot = PlotWidget()
-        self.metrics_high_plot.axes.set_title('Metrics (higher is better)')
-        self.plots.addTab(self.metrics_high_plot, 'Metrics (correlations)')
+        final_metrics_high_widget, self.final_metrics_high_plot, self.final_metrics_high_table = plot_widget_and_table()
+        self.final_metrics_high_plot.axes.set_title('Metrics (higher is better)')
+        self.plots.addTab(final_metrics_high_widget, 'Final Metrics (correlations)')
 
         self.low_metrics = set()
         self.high_metrics = set()
@@ -75,63 +139,76 @@ class ExpCompareDialog(QDialog):
         self.metrics_by_exp[expid] = d['data']
         if len(self.metrics_by_exp) == len(self.expids):
             self.plot_metrics()
+            for expid in self.expids:
+                self.final_metrics_low_groups.add_expid(expid)
 
-    def plot_metrics(self):
-        assert len(self.metrics_by_exp) == len(self.expids)
+    def low_grouping_changed(self, groups: dict):
+        self.low_metrics_grouping = groups
+        self.plot_metrics()
 
-        self.low_metrics = sorted(self.low_metrics)
-        self.high_metrics = sorted(self.high_metrics)
-
-        w = 1. / len(self.expids)
-
-        for i, expid in enumerate(self.expids):
-            colour = f'C{i}'
-            low_x = np.arange(len(self.low_metrics))+np.random.uniform(-0.1, 0.1, len(self.low_metrics))
-            low_y = list([self.metrics_by_exp[expid].get(n, float('nan')) for n in self.low_metrics])
-            self.metrics_low_plot.axes.plot(
-                low_x, low_y, 'o', color=colour
-            )
-
-            high_x = np.arange(len(self.high_metrics))+np.random.uniform(-0.1, 0.1, len(self.high_metrics))
-            high_y = list([self.metrics_by_exp[expid].get(n, float('nan')) for n in self.high_metrics])
-            self.metrics_high_plot.axes.plot(
-                high_x, high_y, 'o', color=colour
-            )
-
-        ne = len(self.expids)
-        low_lbl_x = np.arange(len(self.low_metrics))
-        low_labels = [
+    def plot_metrics_set(self, plot_widget: PlotWidget, table: QTableWidget, metrics_set: list, groupings: dict):
+        table.clear()
+        table.setColumnCount(2)
+        table.setRowCount((len(self.expids)+1)*len(self.low_metrics))
+        labels = [
             # ('\n'*(i % 2)) +
             (t
              .replace('metrics.', '')
              .replace('test', 'Te')
-             .replace('valid', 'V'))
-            for i, t in enumerate(self.low_metrics)
+             .replace('valid', 'V')
+             .replace('MeanAbsoluteError', 'MAE'))
+            for i, t in enumerate(metrics_set)
         ]
-        self.metrics_low_plot.axes.set_xticks(
-            low_lbl_x,
-            low_labels,
-            rotation=45,
-            ha='right', va='top'
-        )
-        self.metrics_low_plot.axes.set_yscale('log')
-        self.metrics_low_plot.redraw_and_flush()
 
-        high_lbl_x = np.arange(len(self.high_metrics))
-        high_labels = [
-            # ('\n' * (i % 2)) +
-            (t
-             .replace('metrics.', '')
-             .replace('test', 'Te')
-             .replace('valid', 'V'))
-            for i, t in enumerate(self.high_metrics)
-        ]
-        self.metrics_high_plot.axes.set_xticks(
-            high_lbl_x,
-            high_labels,
-            rotation=45,
-            ha='right', va='top'
+        plot_widget.clear()
+        if not groupings:
+            for i, expid in enumerate(self.expids):
+                colour = f'C{i}'
+                x = np.arange(len(metrics_set))+np.random.uniform(-0.1, 0.1, len(metrics_set))
+                y = list([self.metrics_by_exp[expid].get(n, float('nan')) for n in metrics_set])
+                plot_widget.axes.plot(
+                    x, y, 'o', color=colour
+                )
+
+            for i, expid in enumerate(self.expids):
+                for j, (k, kk) in enumerate(zip(labels, metrics_set)):
+                    v = self.metrics_by_exp[expid].get(kk, float('nan'))
+                    table.setItem((i+1)*len(self.low_metrics) + j, 0, QTableWidgetItem(f'{expid}/{k}'))
+                    table.setItem((i+1)*len(self.low_metrics) + j, 1, QTableWidgetItem(f'{v}'))
+
+            for i, k in enumerate(metrics_set):
+                v = sum([self.metrics_by_exp[e].get(k, float('nan')) for e in self.expids])/len(self.expids)
+                table.setItem(i, 0, QTableWidgetItem(f'mean/{k}'))
+                table.setItem(i, 1, QTableWidgetItem(f'{v}'))
+                plot_widget.axes.plot(
+                    [i-0.2, i+0.2], [v, v], '--', color='k'
+                )
+        else:
+            exp_by_group = {}
+            for expid, group in groupings.items():
+                if group not in exp_by_group:
+                    exp_by_group[group] = list()
+                exp_by_group[group].append(expid)
+
+            for i, group in enumerate(sorted(exp_by_group)):
+                expids = exp_by_group[group]
+                x, y = [], []
+                for exp in expids:
+                    for j, m in enumerate(metrics_set):
+                        x.append(j)
+                        y.append(self.metrics_by_exp[exp].get(m, float('nan')))
+                plot_widget.axes.plot(
+                    x, y, 'o', color=f'C{i}'
+                )
+        lbl_x = np.arange(len(metrics_set))
+        plot_widget.axes.set_xticks(
+            lbl_x, labels,
         )
-        self.metrics_high_plot.redraw_and_flush()
+        plot_widget.axes.set_yscale('log')
+        plot_widget.redraw_and_flush()
+
+    def plot_metrics(self):
+        assert len(self.metrics_by_exp) == len(self.expids)
+        self.plot_metrics_set(self.final_metrics_low_plot, self.final_metrics_low_table, sorted(self.low_metrics), self.low_metrics_grouping)
 
 
