a9ad546d70a5c50de4588dc41bccbb7c6db2b248
Christopher Boyle <chrismjboyle@gmail.com>
2023-02-06 11:47:29 +0000

    change requires to fix sklearn/scikit-learn bug

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mldb',
-    version='1.0',
-    description='ML Experiment tracker database facilitation',
-    author='Chris Boyle',
-    author_email='chris@cmjb.tech',
+    name="mldb",
+    version="1.0",
+    description="ML Experiment tracker database facilitation",
+    author="Chris Boyle",
+    author_email="chris@cmjb.tech",
     packages=find_packages(),
-    install_requires=['wheel'],
-    requires=['numpy', 'matplotlib', 'psycopg2', 'simplejson', 'syt', 'scipy', 'scikit-learn'],
-    extras_require=dict(
-        test=['pytest', 'torch'],
-        qtui='PySide6'
-    )
+    install_requires=[
+        "wheel",
+        "numpy",
+        "matplotlib",
+        "psycopg2",
+        "simplejson",
+        "syt",
+        "scipy",
+        "scikit-learn",
+    ],
+    extras_require=dict(test=["pytest", "torch"], qtui="PySide6"),
 )
